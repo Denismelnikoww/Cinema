29be22d452e7514308148f6bdf7bef437890d299
да <denismelnikoww@gmail.com>
2025-06-14 15:29:27 +0300

    бизнес логика создания сеансов и бронирования

--- Cinema.Application/UseCases/Booking/CreateBookingUseCase.cs
+++ Cinema.Application/UseCases/Booking/CreateBookingUseCase.cs
@@ -13,31 +13,66 @@ namespace Cinema.Application.UseCases.Booking
     public class CreateBookingUseCase
     {
         private readonly IBookingRepository _bookingRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly ISessionRepository _sessionRepository;
+        private readonly IHallRepository _hallRepository;
         private readonly IValidator<BookingDto> _validator;
 
         public CreateBookingUseCase(IBookingRepository bookingRepository,
-            IValidator<BookingDto> validator)
+            IValidator<BookingDto> validator,
+            IUserRepository userRepository,
+            ISessionRepository sessionRepository,
+            IHallRepository hallRepository)
         {
             _bookingRepository = bookingRepository;
-            _validator = validator; 
+            _validator = validator;
+            _userRepository = userRepository;
+            _sessionRepository = sessionRepository;
+            _hallRepository = hallRepository;
         }
 
-        public async Task<Result<string>> ExecuteAsync(BookingDto bookingDto, 
+        public async Task<Result<string>> ExecuteAsync(BookingDto bookingDto,
             CancellationToken cancellationToken)
         {
             var validateResult = await _validator.ValidateAsync(bookingDto,
                 cancellationToken);
-            
+
             if (!validateResult.IsValid)
             {
                 return Error.BadRequest(JsonSerializer
                     .Serialize(validateResult.ToDictionary()));
             }
 
+            var user = await _userRepository.FindAsync(bookingDto.UserId, cancellationToken);
+            if (user == null || user.IsDeleted)
+            {
+                return Error.BadRequest("The user does not exist");
+            }
+
+            var session = await _sessionRepository.FindAsync(bookingDto.SessionId,
+                cancellationToken);
+            if (session == null || session.IsDeleted)
+            {
+                return Error.BadRequest("The session does not exist");
+            }
+
+            var hall = await _hallRepository.FindAsync(session.HallId, cancellationToken);
+            if (hall.CountSeats < bookingDto.SeatNumber)
+            {
+                return Error.BadRequest("This place does not exist");
+            }
+
+            var bookings = await _bookingRepository.FindBySessionAsync(bookingDto.SessionId,
+                cancellationToken);
+            if (bookings.Any(b => b.SeatNumber == bookingDto.SeatNumber))
+            {
+                return Error.BadRequest("This place is already occupied");
+            }
+
             await _bookingRepository.AddAsync(bookingDto.SessionId,
-                                         bookingDto.UserId,
-                                         bookingDto.SeatNumber,
-                                         cancellationToken);
+                                             bookingDto.UserId,
+                                             bookingDto.SeatNumber,
+                                             cancellationToken);
 
             return Result.Success("Booking successful create");
         }
--- Cinema.Application/UseCases/Session/CreateSessionUseCase.cs
+++ Cinema.Application/UseCases/Session/CreateSessionUseCase.cs
@@ -10,12 +10,19 @@ namespace Cinema.Application.UseCases.Session
     public class CreateSessionUseCase
     {
         private readonly ISessionRepository _sessionRepository;
+        private readonly IMovieRepository _movieRepository;
+        private readonly IHallRepository _hallRepository;
         private readonly IValidator<SessionDto> _validator;
 
-        public CreateSessionUseCase(ISessionRepository sessionRepository, IValidator<SessionDto> validator)
+        public CreateSessionUseCase(ISessionRepository sessionRepository,
+            IMovieRepository movieRepository,
+            IHallRepository hallRepository,
+            IValidator<SessionDto> validator)
         {
             _sessionRepository = sessionRepository;
             _validator = validator;
+            _movieRepository = movieRepository;
+            _hallRepository = hallRepository;
         }
 
         public async Task<Result<string>> ExecuteAsync(SessionDto sessionDto,
@@ -26,6 +33,37 @@ namespace Cinema.Application.UseCases.Session
             if (!validateResult.IsValid)
             {
                 return Error.BadRequest(JsonSerializer.Serialize(validateResult.ToDictionary()));
+            }
+
+            var movie = await _movieRepository.FindAsync(sessionDto.MovieId, cancellationToken);
+
+            if (movie == null || movie.IsDeleted)
+            {
+                return Error.BadRequest("The movie does not exist");
+            }
+            if (movie.Duration > sessionDto.Duration)
+            {
+                return Error.BadRequest("The duration of the session " +
+                    "cannot be less than a movie.");
+            }
+
+            var hall = await _hallRepository.FindAsync(sessionDto.HallId, cancellationToken);
+
+            if (hall == null || hall.IsDeleted)
+            {
+                return Error.BadRequest("There is no hall with such an ID");
+            }
+            if (!hall.IsWorking)
+            {
+                return Error.BadRequest("This hall is temporarily/permanently closed");
+            }
+
+            var sessions = await _sessionRepository.GetAllByHallAsync(hall.Id, cancellationToken);
+            if (sessions.Any(s =>
+                   (sessionDto.DateTime < s.DateTime + s.Duration) &&
+                   (sessionDto.DateTime + sessionDto.Duration > s.DateTime)))
+            {
+                return Error.BadRequest("The session time is adjusted to another session");
             }
 
             await _sessionRepository.AddAsync(sessionDto.MovieId,
